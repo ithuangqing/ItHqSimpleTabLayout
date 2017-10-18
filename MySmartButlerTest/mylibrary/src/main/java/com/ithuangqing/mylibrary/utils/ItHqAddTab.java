90a15549e97db43d8d32d35326c6d117070faf2f
ithuangqing <ithuangqing@gmail.com>
2017-10-18 15:06:54 +0800

    更改
    添加库

---
+++ MySmartButlerTest/mylibrary/src/main/java/com/ithuangqing/mylibrary/utils/ItHqAddTab.java
@@ -0,0 +1,48 @@
+package com.ithuangqing.mylibrary.utils;
+
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+/**
+ * @author ithuangqing
+ * @data 2017/10/17.
+ */
+
+public class ItHqAddTab {
+
+    public  static   void addTab(TabLayout tabLayout, ViewPager viewPager, final List<Fragment> fragment, final List<String> title, FragmentManager fragmentManager){
+
+        /**
+         * 预加载
+         */
+        viewPager.setOffscreenPageLimit(fragment.size());
+
+        viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragment.get(position);
+            }
+            @Override
+            public int getCount() {
+                return fragment.size();
+            }
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return title.get(position);
+            }
+        });
+
+       // TabLayout关联ViewPager
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+
+}
